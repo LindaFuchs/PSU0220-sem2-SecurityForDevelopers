e8acf53a736a4608fa0ce2da6afbefe0ee6e8732
LindaFuchs <dev@lindafuchs.net>
2020-11-22 22:33:23 +0100

    Initial Commit of BruteForceAttack Seperation of concerns, created AttackSequence and PasswordList + AttackSequence : contains generic sequence of the attack + PasswordList : Sinple list with passwords to try
    Added NuGet package Dapplo.Windows.Input to simulate input

---
+++ BruteForceAttack/AttackSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BruteForceAttack
+{
+    class AttackSequence
+    {
+        public AttackSequence()
+        {
+            AttackStart();
+            AttackMain();
+            AttackEnd();
+
+
+        }
+
+
+        private void AttackStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AttackMain()
+        {
+            PasswordList passwordList = new PasswordList();
+        }
+
+        private void AttackEnd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}
---
+++ BruteForceAttack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForceAttack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = new AttackSequence();
+        }
+    }
+}
