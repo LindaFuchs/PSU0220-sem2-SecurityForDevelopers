6cfcc2cde48e8b31ece0d8558ff9addb3f8031ce
LindaFuchs <dev@lindafuchs.net>
2020-11-22 14:32:47 +0100

    + UserDetails : string array of user details to be used for valid logins. + LoginLogic : basic empty class, to be used for holding login logic. + BruteForce : string array of passwords, to be used for the attack. + few details in main to start the sequence for logging in and initializing the classes and data.

---
+++ psu0220-Sem-2-SecurityForDevelopers/BruteForce.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psu0220_Sem_2_SecurityForDevelopers
+{
+    class BruteForce
+    {
+        public string[] passwordList;
+
+        public BruteForce()
+        {
+            passwordList = new string[] {   "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                                            "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN",
+                                            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                                            "fourtytwo", "FOURTYTWO", "42",
+                                            "sixhundredandsixtysix", "SIXHUNDREDANDSIXTYSIX", "666",
+                                            "MonkeyBanana", "HeartShapedPear", "AccessDenied" };
+        }
+
+        public void StartAttack()
+        {
+
+        }
+
+
+    }
+}
---
+++ psu0220-Sem-2-SecurityForDevelopers/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psu0220_Sem_2_SecurityForDevelopers
+{
+    class UserDetails
+    {
+        public string[,] validLogins;
+
+        public UserDetails()
+        {
+            validLogins = new string[,] { { "admin", "42" }, { "user", "MonkeyBanana" } };
+        }
+
+
+
+    }
+}
