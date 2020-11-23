e47e234610feacf2ff0dd6e86d5106c8a0531df5
LindaFuchs <dev@lindafuchs.net>
2020-11-23 16:26:40 +0100

    V2.0 Working Login console app. Working brute force app. Still need to manually switch focus to the log in app for the hack to work.

--- BruteForceAttack/AttackLogic.cs
+++ BruteForceAttack/AttackLogic.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsInput;
+
+
 
 namespace BruteForceAttack
 {
     class AttackLogic
     {
+        private InputSimulator inputSimulator = new InputSimulator();
+
 
         /// <summary>
         /// Asks confirmation to start the attack.
@@ -31,57 +36,60 @@ namespace BruteForceAttack
                     Console.WriteLine("Try again.");
                     Console.WriteLine("Type 'Execute' to begin the attack." + "\n ");
                     input = Console.ReadLine();
-                    
                 }
             } while (!proceed);
         }
 
-
-
         /// <summary>
         /// Countdown method, counts down 5 seconds.
         /// </summary>
-        public bool CountDown()
+        public void CountDown()
         {
-            bool b = false;
+            Console.WriteLine("Attack starting in 5");
+            Task.WaitAll(Task.Delay(1000));
 
-            b = fiveseconds();
+            Console.WriteLine("Attack starting in 4");
+            Task.WaitAll(Task.Delay(1000));
 
-            bool fiveseconds()
-            {
-                Console.WriteLine("Attack starting in 5");
-                Task.WaitAll(Task.Delay(1000));
+            Console.WriteLine("Attack starting in 3");
+            Task.WaitAll(Task.Delay(1000));
 
-                Console.WriteLine("Attack starting in 4");
-                Task.WaitAll(Task.Delay(1000));
+            Console.WriteLine("Attack starting in 2");
+            Task.WaitAll(Task.Delay(1000));
 
-                Console.WriteLine("Attack starting in 3");
-                Task.WaitAll(Task.Delay(1000));
+            Console.WriteLine("Attack starting in 1");
+            Task.WaitAll(Task.Delay(1000));
 
-                Console.WriteLine("Attack starting in 2");
-                Task.WaitAll(Task.Delay(1000));
+            Console.WriteLine("Commencing Attack");
+            Task.WaitAll(Task.Delay(1000));
 
-                Console.WriteLine("Attack starting in 1");
-                Task.WaitAll(Task.Delay(1000));
-
-                Console.WriteLine("Commencing Attack");
-                Task.WaitAll(Task.Delay(1000));
-
-                return true;
-            }
-
-            do
-            {
-
-            } while (!b);
-
-            return b;
 
         }
 
-        internal void DoAttack()
+
+
+        internal bool DoAttack(PasswordList passwordList)
         {
-            Console.WriteLine("Attack pewpew"); ;
+            Console.WriteLine("Attack pewpew Laz0rz !!!");
+            Console.WriteLine("========================");
+            return SimulateTextEntry(passwordList);
+        }
+
+
+
+
+        private bool SimulateTextEntry(PasswordList passwordList)
+        {
+            KeyboardSimulator keyboardSimulator = new KeyboardSimulator(inputSimulator);
+
+            foreach (string pass in passwordList.passwordList)
+            {
+                keyboardSimulator.TextEntry(pass);
+                inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.RETURN);
+            }
+
+            return false;
+
         }
     }
 }
